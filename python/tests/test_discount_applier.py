207fd413a2ef2d3a5aaa638d0ff442372b2a8add
emily <emily@bacheconsulting.com>
2023-12-14 19:26:52 +0100

    sample solution python

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,11 +1,28 @@
-from discount_applier import DiscountApplier
+from discount_applier import DiscountApplier, Notifier
+from discount_applier import User
+
+
+class SpyNotifier(Notifier):
+    def __init__(self):
+        self.notified = []
+
+    def notify(self, user, message):
+        self.notified.append(user.name)
 
 
 def test_apply_v1():
-    # TODO: trigger the bug in DiscountApplier.ApplyV1() by providing an implemention of the Notifier interface
-    assert False
+    notifier = SpyNotifier()
+    user1 = User("user1")
+    discount_applier = DiscountApplier(notifier)
+    discount_applier.apply_v1(100, [user1])
+    assert user1.name in notifier.notified
 
 
 def test_apply_v2():
-    # TODO: trigger the bug in DiscountApplier.ApplyV2() by providing an implementation of the Notifier interface
-    assert False
+    notifier = SpyNotifier()
+    user1 = User("user1")
+    user2 = User("user2")
+    discount_applier = DiscountApplier(notifier)
+    discount_applier.apply_v2(100, [user1, user2])
+    assert user1.name in notifier.notified
+    assert user2.name in notifier.notified
