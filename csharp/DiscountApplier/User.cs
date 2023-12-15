c13bb34c1c9024f19075f63c5c1e04c8064f2fba
emily <emily@bacheconsulting.com>
2023-12-15 11:46:03 +0100

    cosmetic

--- csharp/DiscountApplier/User.cs
+++ csharp/DiscountApplier/User.cs
@@ -1,14 +1,13 @@
-﻿namespace DiscountApplier
-{
-    public readonly struct User
-    {
-        public User(string name, string email)
-        {
-            Name = name;
-            Email = email;
-        }
+﻿namespace DiscountApplier;
 
-        public string Name { get; init; }
-        public string Email { get; init; }
+public readonly struct User
+{
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
     }
+
+    public string Name { get; init; }
+    public string Email { get; init; }
 }
