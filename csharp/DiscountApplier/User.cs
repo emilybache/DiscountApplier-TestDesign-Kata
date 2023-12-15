5bb76b769a8bb742a4726d07d583d73a15cc600e
emily <emily@bacheconsulting.com>
2023-12-15 10:18:24 +0100

    add constructor for convenience

--- csharp/DiscountApplier/User.cs
+++ csharp/DiscountApplier/User.cs
@@ -2,6 +2,12 @@
 {
     public readonly struct User
     {
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
         public string Name { get; init; }
         public string Email { get; init; }
     }
