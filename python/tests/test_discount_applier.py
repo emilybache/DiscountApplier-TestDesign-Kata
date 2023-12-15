cdb2ea53066aaca1bb26ba415554e3d03b970820
emily <emily@bacheconsulting.com>
2023-12-15 10:02:46 +0100

    Revert "sample solution python"
    This reverts commit 207fd413a2ef2d3a5aaa638d0ff442372b2a8add.

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,28 +1,11 @@
-from discount_applier import DiscountApplier, Notifier
-from discount_applier import User
-
-
-class SpyNotifier(Notifier):
-    def __init__(self):
-        self.notified = []
-
-    def notify(self, user, message):
-        self.notified.append(user.name)
+from discount_applier import DiscountApplier
 
 
 def test_apply_v1():
-    notifier = SpyNotifier()
-    user1 = User("user1")
-    discount_applier = DiscountApplier(notifier)
-    discount_applier.apply_v1(100, [user1])
-    assert user1.name in notifier.notified
+    # TODO: trigger the bug in DiscountApplier.ApplyV1() by providing an implemention of the Notifier interface
+    assert False
 
 
 def test_apply_v2():
-    notifier = SpyNotifier()
-    user1 = User("user1")
-    user2 = User("user2")
-    discount_applier = DiscountApplier(notifier)
-    discount_applier.apply_v2(100, [user1, user2])
-    assert user1.name in notifier.notified
-    assert user2.name in notifier.notified
+    # TODO: trigger the bug in DiscountApplier.ApplyV2() by providing an implementation of the Notifier interface
+    assert False
