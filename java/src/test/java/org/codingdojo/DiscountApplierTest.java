565e6f68d0d546e9cfa3ec8acd6641554404f44d
emily <emily@bacheconsulting.com>
2024-07-05 15:40:56 +0200

    sample solution in java

--- java/src/test/java/org/codingdojo/DiscountApplierTest.java
+++ java/src/test/java/org/codingdojo/DiscountApplierTest.java
@@ -2,15 +2,35 @@ package org.codingdojo;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Array;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class DiscountApplierTest {
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v1() {
-    // TODO: trigger the bug in DiscountApplier.applyV1() by implementing the Notifier interface
+    var notifier = mock(Notifier.class);
+    var sut = new DiscountApplier(notifier);
+    var user1 = new User("user1", "user1@example.com");
+
+    sut.applyV1(10.0, List.of(user1));
+
+    verify(notifier).notify(user1, "You've got a new discount of 10,000000");
   }
 
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v2() {
-    // TODO: trigger the bug in DiscountApplier.applyV2() by implementing Notifier interface
+    var notifier = mock(Notifier.class);
+    var sut = new DiscountApplier(notifier);
+    var user1 = new User("user1", "user1@example.com");
+    var user2 = new User("user2", "user2@example.com");
+
+    sut.applyV1(10.0, List.of(user1, user2));
+
+    verify(notifier).notify(user1, "You've got a new discount of 10,000000");
+    verify(notifier).notify(user2, "You've got a new discount of 10,000000");
   }
 
 }
