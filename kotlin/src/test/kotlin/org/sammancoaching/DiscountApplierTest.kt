c89b559bc715669a8f7460a0dd4f20109c3dec6d
emily <emily@bacheconsulting.com>
2025-10-02 15:52:11 +0200

    use relaxed mocks

--- kotlin/src/test/kotlin/org/sammancoaching/DiscountApplierTest.kt
+++ kotlin/src/test/kotlin/org/sammancoaching/DiscountApplierTest.kt
@@ -1,19 +1,19 @@
 package org.sammancoaching
 
-import io.mockk.every
 import io.mockk.verify
 import io.mockk.mockk
-import java.util.List
 import kotlin.test.Test
 
 class DiscountApplierTest {
+    val notifier: Notifier = mockk(relaxed = true)
+    val user1 = User("user1", "user1@example.com")
+    val user2 = User("user2", "user2@example.com")
+
     @Test
     fun should_notify_when_applying_discount_for_user_v1() {
-        val notifier: Notifier = mockk()
-        val sut = DiscountApplier(notifier)
-        val user1 = User("user1", "user1@example.com")
+        val discountApplier = DiscountApplier(notifier)
 
-        sut.applyV1(10.0, listOf<User>(user1))
+        discountApplier.applyV1(10.0, listOf(user1))
 
         verify {
             notifier.notify(user1, "You've got a new discount of 10,000000")
@@ -22,12 +22,9 @@ class DiscountApplierTest {
 
     @Test
     fun should_notify_twice_when_applying_discount_for_two_users_v2() {
-        val notifier: Notifier = mockk()
-        val sut = DiscountApplier(notifier)
-        val user1 = User("user1", "user1@example.com")
-        val user2 = User("user2", "user2@example.com")
+        val discountApplier = DiscountApplier(notifier)
 
-        sut.applyV1(10.0, List.of<User>(user1, user2))
+        discountApplier.applyV1(10.0, listOf(user1, user2))
 
         verify {
             notifier.notify(user1, "You've got a new discount of 10,000000")
